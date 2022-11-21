56642cbc3079c28534c12c619a2ebe74b8cf65e6
dolyadima <dimchik96@gmail.com>
2022-11-21 15:09:16 +0200

    fix namespace urls.py, add app_name

--- acs_store/urls.py
+++ acs_store/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 
+app_name = 'acs_store'
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include("products.urls"), namespace="products"),
+    path("admin/", admin.site.urls),
+    path("", include("products.urls", namespace="products")),
 ]
--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import ProductListView
 
 
+app_name = 'products'
 urlpatterns = [
     path('', ProductListView.as_view(), name="product-list"),
 ]
