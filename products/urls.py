9225fe35bb35f8eedd16f0ac4e76bc2656f22e98
vera-ab <verunja108@gmail.com>
2022-11-22 22:47:09 +0200

    navigation menu, create view and page added

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import ProductListView, SalesListView
-
+from .views import ProductListView, SalesListView, SaleCreateView
 
 app_name = 'products'
 urlpatterns = [
     path('', ProductListView.as_view(), name="product-list"),
     path('sales_list/', SalesListView.as_view(), name='sales-list'),
+    path('sale_create/', SaleCreateView.as_view(), name='sale-create'),
 ]
